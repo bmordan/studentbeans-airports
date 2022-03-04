309e5c32f924069d0c7ffbe379cbc0e27ee7b981
Bernard Mordan <bernardmordan@gmail.com>
2022-03-04 12:12:06 +0000

    async await

--- Airport.js
+++ Airport.js
@@ -2,23 +2,16 @@ const fs = require('fs')
 const path = require('path')
 
 class Airport {
+    static allAirports = {}
+
     constructor(code) {
         this.code = code
+        Airport.allAirports[code] = this
     }
-
-    // getInfo(cb) {
-    //     fs.readFile(path.join(__dirname, '..', 'airport-data.json'), (err, buffer) => {
-    //         if (err) throw err
-    //         const info = JSON.parse(buffer.toString())
-    //           .find(airport => airport.code === this.code)
-    //         this.city = info.city
-    //         cb(info)
-    //     })
-    // }
 
     getInfo() {
         return new Promise((resolve, reject) => {
-            fs.readFile(path.join(__dirname, '..', 'airport-data.json'), (err, buffer) => {
+            fs.readFile(path.join(__dirname, 'airports.json'), (err, buffer) => {
                 if (err) return reject(err)
                 const info = JSON.parse(buffer.toString())
                 .find(airport => airport.code === this.code)
@@ -26,6 +19,17 @@ class Airport {
                 resolve(info)
             })           
         })
+    }
+
+    takeOff(plane) {
+        // plane.inBound "LAX"
+        // remove this plane from this airport instance
+        Airports.allAirports[plane.inBound].land(plane)
+
+    }
+
+    land(plane) {
+        this.planes.push(plane)
     }
 }
 
--- Airport.test.js
+++ Airport.test.js
@@ -1,12 +1,26 @@
 const Airport = require('./Airport')
 
 describe("Airport", () => {
-    test("airport has info", (done) => {
+    beforeEach(() => {
+        Airport.allAirports = {}
+    })
+
+    test("airport has info", async () => {
         const airport = new Airport("QQD")
-        airport.getInfo((info) => {
-            expect(info.city).toBeTruthy()
-            expect(info.city).toBe("Dover")
-            done()
-        })
+        const info = await airport.getInfo()
+        expect(info.city).toBe("Dover")
+    })
+    test("airports know about each other", () => {
+        const airport = new Airport("LHR")
+        const other_airport = new Airport("LAX")
+        expect(typeof Airport.allAirports).toBe('object')
+        expect(airport.allAirports).toBeFalsy()
+        expect(Airport.allAirports.LAX instanceof Airport).toBeTruthy()
+        expect(Airport.allAirports.LAX).toEqual(other_airport)
+    })
+    test("airports know about each other", () => {
+        const airport = new Airport("BEL")
+        const other_airport = new Airport("GAT")
+        expect(Object.keys(Airport.allAirports).length).toBe(2)
     })
 })
